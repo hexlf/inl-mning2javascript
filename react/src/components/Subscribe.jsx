fe56802c92933f2ff21aeec50e6aaf691ef93916
ZelFS <86790952+hexlf@users.noreply.github.com>
2025-11-13 22:26:55 +0100

    Done with subscribe api post

--- react/src/components/Subscribe.jsx
+++ react/src/components/Subscribe.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Subscribe() {
+export default function Subscribe() {
+  const [data, setData] = useState({email:''})
+
+  const handleInputChange = (e) => {
+    const {name, value } = e.target;
+    setData({ ...data, [name]: value }); 
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch('https://win25-jsf-assignment.azurewebsites.net/api/subscribe', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    console.log(data)
+
+  };
+
   return (
     <div className='Subscribe-area'>
       <div className='Subscribe-textside'>
@@ -9,13 +31,11 @@ function Subscribe() {
            updates and info</p>
       </div>
       <div className='Subscribe-submit'> 
-        <form>
-          <input className='textinput' type="email" required placeholder="Enter your email" />
+        <form onSubmit={handleSubmit}>
+          <input className='textinput' name='email' type="email" value={data.email} onChange={handleInputChange} required placeholder="Enter your email" />
           <button className='NeedToBeYelowbtn' type="submit">submit</button>
         </form>
       </div>
     </div>
   )
 }
-
-export default Subscribe
